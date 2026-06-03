d51641bdd3d8efcd82a9f2b271a23d42df1059c4
choichoi3227-crypto <choichoi3227@gmail.com>
2026-06-03 11:37:30 +0900

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://cloudpress.kr',
+  site: 'https://bloggerskin-wanttomakeitastro.hoi3227.workers.dev',
   output: 'server',
   adapter: cloudflare({
-    imageService: 'compile'
+    imageService: 'passthrough',
   }),
+  security: {
+    checkOrigin: false,
+  },
   vite: {
     ssr: {
       external: ['node:crypto']
